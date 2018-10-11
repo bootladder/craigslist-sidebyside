fcfaf80fb0c348826414edf5815a4169d932fc4e
steve <steve@bootladder.com>
2018-10-10 18:45:17 -0700

    server can read data from a POST

--- main.go
+++ main.go
@@ -1,10 +1,96 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+type messageHandler struct {
+	message string
+}
+
+func (m *messageHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, m.message)
+}
+
+func messageHandlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hellomessagefunc")
+}
+
+func createMessageHandlerFunc(msg string) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, msg)
+	})
+}
+
+type Note struct {
+	Hello     string    `json:"hello"`
+	SearchURL string    `json:"searchURL"`
+	CreatedOn time.Time `json:"createdon"`
+}
+
+//HTTP Get - /api/notes
+func GetNoteHandler(w http.ResponseWriter, r *http.Request) {
+	var messages []string
+	messages = append(messages, "arr1")
+	messages = append(messages, "arr2")
+	messages = append(messages, "arr3")
+	messages = append(messages, "arr4")
+
+	w.Header().Set("Content-Type", "application/json")
+	j, err := json.Marshal(messages)
+	if err != nil {
+		panic(err)
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(j)
+}
+
+//HTTP Post - /api/notes
+func PostNoteHandler(w http.ResponseWriter, r *http.Request) {
+	var note Note
+	// Decode the incoming Note json
+	err := json.NewDecoder(r.Body).Decode(&note)
+	if err != nil {
+		panic(err)
+	}
+	note.CreatedOn = time.Now()
+	//	k := strconv.Itoa(id)
+	//	noteStore[k] = note
+	j, err := json.Marshal(note)
+	if err != nil {
+		panic(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write(j)
+}
 
 func main() {
+	r := mux.NewRouter().StrictSlash(false)
+	r.HandleFunc("/api/notes", GetNoteHandler).Methods("GET")
+	r.HandleFunc("/api/notes", PostNoteHandler).Methods("POST")
+
+	http.ListenAndServe(":8080", r)
+}
+
+func main2() {
 	mux := http.NewServeMux()
 	fs := http.FileServer(http.Dir("public"))
 	mux.Handle("/", fs)
+	mux.Handle("/welcome", &messageHandler{"hello123"})
+
+	mux.Handle("/func", http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "super lambda2")
+		}))
+
+	mux.Handle("/closure", createMessageHandlerFunc("my closure thing"))
+
 	http.ListenAndServe(":8080", mux)
 }
---
+++ public/app.jsx
@@ -0,0 +1,75 @@
+class CraigslistResults extends React.Component {
+  render() {
+    return (
+    <div>{this.props.hello}</div>
+    );
+  }
+}
+class CraigslistQueryColumn extends React.Component {
+  state = {
+    message: "initial state",
+    users: [],
+    error: null
+  }
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.props.onInputEvent(e.target.value);
+  }
+
+  componentDidMount() {
+
+    const myURL="https://detroit.craigslist.org/d/architect-engineer-cad/search/egr"
+
+    //fetch("http://localhost:8080/api/" + encodeURIComponent(myURL), {
+    fetch("http://localhost:8080/api" , {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: {"searchURL":"myvalue321432"}
+    })
+    .then(response => response.json())
+    .then(data =>
+      this.setState({
+        users: data,
+        message: "ok!!",
+      })
+    )
+    .catch(error => this.setState({ error: JSON.stringify(error), message: "something bad happened"+JSON.stringify(error.message) }));
+  }
+
+  render() {
+    return (
+      <div>
+        <form action="/action_page.php">
+          URL: <input type="text" name="url"/><br/>
+        </form> 
+        <button />
+        <input value={37} onChange={this.handleChange} />
+        <div> {JSON.stringify(this.state.users)} and , {JSON.stringify(this.state.message)}</div>
+        <CraigslistResults hello="hello" />
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  handleQueryInput(e) {
+    console.log("hello"+e)
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello JSX and the World!</h1>
+        <CraigslistQueryColumn onInputEvent={this.handleQueryInput}/>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
