3e5929cc59d8d8cc5f214260a865c6c2d9981b7c
steve <steve@bootladder.com>
2018-10-31 23:19:22 -0700

    urlstore

--- urlstore.go
+++ urlstore.go
@@ -1,6 +1,37 @@
 package main
 
+import (
+	"encoding/json"
+	"io"
+)
+
 type urlStore struct {
+	reader  io.Reader
+	urlsets []urlSet
+}
+type urlSet struct {
+	urls []string
+}
+
+func (s *urlStore) loadURLs() error {
+	var b []byte
+	n, err := s.reader.Read(b)
+	printf("the url string is : %v and number of bytes read is %v", b, n)
+	if err != nil {
+		return err
+	}
+
+	err = s.parseURLsFile(b)
+	return err
+}
+
+func (s *urlStore) parseURLsFile(b []byte) error {
+
+	urlsets := make([]urlSet, 0)
+	err := json.Unmarshal(b, urlsets)
+	urlstore.urlsets = urlsets
+	return err
+	//return errors.New("hello")
 }
 
 func (s *urlStore) saveURL(url string) {
@@ -18,9 +49,6 @@ func (s *urlStore) addURL() {
 
 func (s *urlStore) getUrls() []string {
 	return nil
-}
-
-func (s *urlStore) loadURLs() {
 }
 
 func (s *urlStore) loadURLSet2() {
--- urlstore_test.go
+++ urlstore_test.go
@@ -1,7 +1,94 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"testing"
+)
 
-func TestSum(t *testing.T) {
-	t.Errorf("Sum was incorrect, got: %d, want: %d.", 39, 10)
+func Test_loadURLs_readFailed_returnsError(t *testing.T) {
+	var urlstore urlStore
+	mockreader := mockReader{}
+	mockreader.shouldError = true
+	urlstore.reader = mockreader
+
+	err := urlstore.loadURLs()
+	if err == nil {
+		t.Errorf("expected file io error, got nil")
+	}
+}
+func Test_loadURLs_badJSON_returnsError(t *testing.T) {
+	var urlstore urlStore
+	mockreader := mockReader{}
+	mockreader.shouldError = false
+	mockreader.returnedBytes = []byte("hello")
+	urlstore.reader = mockreader
+
+	err := urlstore.loadURLs()
+	if err == nil {
+		t.Errorf("expected file io error, got nil")
+	}
+}
+
+func Test_loadURLs_goodJSON_OK(t *testing.T) {
+
+	jsonString := `
+	{
+		"arra": [
+			"http://boston.craigslist.org/jjj/?query=hello",
+			"http://portland.craigslist.org/jjj/?query=hello"
+		],
+		"arr": [
+			"http://austin.craigslist.org/jjj/?query=hello",
+			"http://houston.craigslist.org/jjj/?query=hello"
+		]
+	}
+		`
+	var urlstore urlStore
+	mockreader := mockReader{}
+	mockreader.shouldError = false
+	mockreader.returnedBytes = []byte(jsonString)
+	urlstore.reader = mockreader
+
+	err := urlstore.loadURLs()
+	if err != nil {
+		t.Errorf("expected no error, got error: " + err.Error())
+	}
+
+	//expected := urlstore.urlsets[0].urls[0]
+	//if expected != "http://boston.craigslist.org/jjj/?query=hello" {
+	//	t.Errorf("expected string got no string")
+	//}
+}
+func Test_loadURLs_topJSONIsNotArray_Fails(t *testing.T) {
+
+	jsonString := `
+	{
+		"validjson" : "butNotAnArray"
+	}
+	
+	`
+	var urlstore urlStore
+	mockreader := mockReader{}
+	mockreader.shouldError = false
+	mockreader.returnedBytes = []byte(jsonString)
+	urlstore.reader = mockreader
+
+	err := urlstore.loadURLs()
+	if err != nil {
+		t.Errorf("expected no error, got error")
+	}
+
+}
+
+type mockReader struct {
+	shouldError   bool
+	returnedBytes []byte
+}
+
+func (m mockReader) Read(p []byte) (n int, err error) {
+	if m.shouldError == true {
+		return 0, errors.New("hello")
+	}
+	copy(p, m.returnedBytes)
+	return len(p), nil
 }
