98e6feacdd9fa433747ad72aa707717894f169a5
Steve Anderson <steve@bootladder.com>
2019-01-05 02:07:05 -0800

    add bootstrap and start column view

--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,9 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
 import Json.Encode exposing (..)
+import Bootstrap.CDN as CDN
+import Bootstrap.Grid as Grid
+import Bootstrap.Button as Button
 
 -- MAIN
 main =
@@ -40,15 +43,58 @@ update msg model =
 view : Model -> Html Msg
 view model =
   div []
-    [ viewInput "text" "Name" model.name Name
+    [
+      Grid.container []
+        [ CDN.stylesheet
+          ,Grid.row [] [
+                Grid.col [] [ queryColumn ]
+               ,Grid.col [] [ queryColumn ]
+               ,Grid.col [] [ queryColumn ]
+               ,Grid.col [] [ queryColumn ]
+              ]
+        ]
+    , viewInput "text" "Name" model.name Name
     , helloHtml ("yo")
     , text "hello2"
-    , div [ (Html.Attributes.property "innerHTML" (Json.Encode.string "Test <em>em</em> here.")) ] []
-    , div [ (Html.Attributes.property "innerHTML" (Json.Encode.string "wat")) ] []
     , viewInput "password" "Password" model.password Password
     , viewInput "password" "Re-enter Password" model.passwordAgain PasswordAgain
     , viewValidation model
     ]
+
+queryColumn =
+    Grid.container []
+        [
+         Grid.row []
+            [ Grid.col []
+                [ input [ placeholder "URL" ] [] ]
+            ]
+        , Grid.row []
+            [ Grid.col []
+                [ input [ placeholder "Search Query"] [] ]
+            ]
+        , Grid.row [] [ Grid.col [] [ categorySelector ] ]
+        , Grid.row [] [ Grid.col [] [ citySelector ] ]
+        , Grid.row []
+            [
+             Grid.col []
+                 [
+                   Button.button [] [text "Load Results and Save URL"]
+                  ,Button.button [] [text "Delete this column"]
+                 ]
+            ]
+        , Grid.row [] [Grid.col [] [ queryResults ]]
+            ]
+queryResults = text "query results"
+
+categorySelector =  select []
+                      [ option [] [text "Select Category"]
+                      , option [] [text "option 2"]
+                      ]
+
+citySelector = select [] [
+                 option [] [text "Select City"]
+                ,option [] [text "Birminham"]
+               ]
 
 helloHtml : String -> Html msg
 helloHtml str = text (str)
