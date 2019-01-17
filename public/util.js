3591e53c3c537cda192e372bdbb0fdba94ecedfe
Steve Anderson <steve@bootladder.com>
2019-01-17 14:36:46 -0800

    delete react stuff

--- public/util.js
+++
@@ -1,6 +0,0 @@
-console.log("utils.js")
-var globals = {
-    key: 1,
-    currentSetIndex: 0
-}
-console.log("utils.js: globals.key: "+globals.key+" and globals.currentSetIndex: " + globals.currentSetIndex)
